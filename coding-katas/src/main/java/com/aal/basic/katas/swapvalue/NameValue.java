5525e2513e31a21ab4f8acdfc6995f86bb1abb96
IrfanAnsari <irfanaansari@gmail.com>
2013-01-30 14:16:01 +0000

    Program to swap value of two variables without using third variable

---
+++ coding-katas/src/main/java/com/aal/basic/katas/swapvalue/NameValue.java
@@ -0,0 +1,25 @@
+package com.aal.basic.katas.swapvalue;
+
+/**
+ * User: irfanansari
+ * Date: 30/01/2013
+ * Time: 13:53
+ */
+public class NameValue {
+
+    private String name;
+    private int value;
+
+    public NameValue(String name, int value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
---
+++ coding-katas/src/main/java/com/aal/basic/katas/swapvalue/SwapVariableValueTest.java
@@ -0,0 +1,42 @@
+package com.aal.basic.katas.swapvalue;
+
+import org.junit.Test;
+import sun.awt.SunHints;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * User: irfanansari
+ * Date: 30/01/2013
+ * Time: 13:52
+ */
+public class SwapVariableValueTest {
+
+    @Test
+    public void shouldSwapTheValueOfVariables(){
+    //Given
+        ValueSwaper valueSwaper = new ValueSwaper();
+    //When
+        NameValue nameValue1 = new NameValue("var1", 3);
+        NameValue nameValue2 = new NameValue("var2", 5);
+        NameValue swappedValue = valueSwaper.swap(nameValue1, nameValue2);
+    //THen
+
+        assertThat(swappedValue.getName(), is("var1"));
+        assertThat(swappedValue.getValue(), is(5));
+    }
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowIllegalArgumentExceptionForBigNumbers(){
+    //Given
+        ValueSwaper valueSwaper = new ValueSwaper();
+    //When
+        NameValue nameValue1 = new NameValue("var1", Integer.MAX_VALUE/2);
+        NameValue nameValue2 = new NameValue("var2", Integer.MAX_VALUE/2);
+        NameValue swappedValue = valueSwaper.swap(nameValue1, nameValue2);
+    //THen
+
+    }
+}
