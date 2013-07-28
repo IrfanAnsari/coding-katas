0badc84c71dd45aa0b590bfb406dfe6202a407c8
IrfanAnsari <irfanaansari@gmail.com>
2013-07-28 13:36:25 +0100

    Refactored the code to set it up in CI environment provided by travis

--- src/test/java/com/aal/basic/katas/fibonacci/FibonacciNumberGeneratorTest.java
+++ src/test/java/com/aal/basic/katas/fibonacci/FibonacciNumberGeneratorTest.java
@@ -1,10 +1,10 @@
 package com.aal.basic.katas.fibonacci;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 
 public class FibonacciNumberGeneratorTest {
