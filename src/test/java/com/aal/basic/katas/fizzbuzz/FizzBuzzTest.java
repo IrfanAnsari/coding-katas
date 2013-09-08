2b736ee741ddb965b1b3ee911af06312e303c7b2
IrfanAnsari <irfanaansari@gmail.com>
2013-09-08 01:11:49 +0100

    Extracted duplicate code from test

--- src/test/java/com/aal/basic/katas/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/aal/basic/katas/fizzbuzz/FizzBuzzTest.java
@@ -1,5 +1,6 @@
 package com.aal.basic.katas.fizzbuzz;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -11,56 +12,58 @@ import static org.junit.Assert.assertThat;
  * Time: 13:30
  */
 public class FizzBuzzTest {
+    private FizzBuzz fizzBuzz = new FizzBuzz();
+
 
     @Test
-    public void shouldReturn1For1(){
-    //Given
-         FizzBuzz fizzBuzz = new FizzBuzz();
-    //When
-         String fizz = fizzBuzz.getSequence(1);
-    //THen
-         assertThat(fizz, is("1"));
+    public void shouldReturn1For1() {
+        //Given
+
+        //When
+        String fizz = fizzBuzz.getSequence(1);
+        //THen
+        assertThat(fizz, is("1"));
     }
 
 
     @Test
-    public void shouldReturn2For2(){
-    //Given
-         FizzBuzz fizzBuzz = new FizzBuzz();
-    //When
-         String fizz = fizzBuzz.getSequence(2);
-    //THen
-         assertThat(fizz, is("2"));
+    public void shouldReturn2For2() {
+        //Given
+
+        //When
+        String fizz = fizzBuzz.getSequence(2);
+        //THen
+        assertThat(fizz, is("2"));
     }
 
 
     @Test
-    public void shouldReturnFizzFor3(){
-    //Given
-         FizzBuzz fizzBuzz = new FizzBuzz();
-    //When
-         String fizz = fizzBuzz.getSequence(3);
-    //THen
-         assertThat(fizz, is("fizz"));
-    }
+    public void shouldReturnFizzFor3() {
+        //Given
 
-     @Test
-    public void shouldReturnFizzFor5(){
-    //Given
-         FizzBuzz fizzBuzz = new FizzBuzz();
-    //When
-         String fizz = fizzBuzz.getSequence(5);
-    //THen
-         assertThat(fizz, is("buzz"));
+        //When
+        String fizz = fizzBuzz.getSequence(3);
+        //THen
+        assertThat(fizz, is("fizz"));
     }
 
     @Test
-    public void shouldReturnFizzBuzzFor15(){
-    //Given
-         FizzBuzz fizzBuzz = new FizzBuzz();
-    //When
-         String fizz = fizzBuzz.getSequence(15);
-    //THen
-         assertThat(fizz, is("fizzbuzz"));
+    public void shouldReturnFizzFor5() {
+        //Given
+
+        //When
+        String fizz = fizzBuzz.getSequence(5);
+        //THen
+        assertThat(fizz, is("buzz"));
+    }
+
+    @Test
+    public void shouldReturnFizzBuzzFor15() {
+        //Given
+
+        //When
+        String fizz = fizzBuzz.getSequence(15);
+        //THen
+        assertThat(fizz, is("fizzbuzz"));
     }
 }
